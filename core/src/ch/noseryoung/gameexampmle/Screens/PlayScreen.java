1f1c2c2dad2af82761189182899ec5ae02b69e74
Sina <sina.blattmann@nyp.ch>
2020-05-15 10:02:06 +0200

    added mario

--- core/src/ch/noseryoung/gameexampmle/Screens/PlayScreen.java
+++ core/src/ch/noseryoung/gameexampmle/Screens/PlayScreen.java
@@ -1,6 +1,7 @@
 package ch.noseryoung.gameexampmle.Screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
@@ -22,6 +23,7 @@ import com.badlogic.gdx.utils.viewport.Viewport;
 
 import ch.noseryoung.gameexampmle.MyGdxGame;
 import ch.noseryoung.gameexampmle.Scenes.Hud;
+import ch.noseryoung.gameexampmle.Sprites.Mario;
 
 public class PlayScreen implements Screen {
 
@@ -38,18 +40,22 @@ public class PlayScreen implements Screen {
     private World world;
     private Box2DDebugRenderer b2dr;
 
+    private Mario mario;
+
+
     public PlayScreen(MyGdxGame game) {
         this.game = game;
         gameCam = new OrthographicCamera();
-        gamePort = new FitViewport(MyGdxGame.V_WIDTH, MyGdxGame.V_HEIGHT, gameCam);
+        gamePort = new FitViewport(MyGdxGame.V_WIDTH / MyGdxGame.PPM, MyGdxGame.V_HEIGHT / MyGdxGame.PPM, gameCam);
         hud = new Hud(game.batch);
 
         mapLoader = new TmxMapLoader();
         map = mapLoader.load("level1.tmx");
-        renderer = new OrthogonalTiledMapRenderer(map);
+        renderer = new OrthogonalTiledMapRenderer(map, 1 / MyGdxGame.PPM);
         gameCam.position.set(gamePort.getWorldWidth() / 2, gamePort.getWorldHeight() / 2, 0);
-        world = new World(new Vector2(0, 0), true);
+        world = new World(new Vector2(0, -10), true);
         b2dr = new Box2DDebugRenderer();
+        mario = new Mario(world);
 
         BodyDef bDef = new BodyDef();
         PolygonShape shape = new PolygonShape();
@@ -59,11 +65,11 @@ public class PlayScreen implements Screen {
         for (MapObject object : map.getLayers().get(2).getObjects().getByType(RectangleMapObject.class)) {
             Rectangle rect = ((RectangleMapObject) object).getRectangle();
             bDef.type = BodyDef.BodyType.StaticBody;
-            bDef.position.set(rect.getX() + rect.getWidth() / 2, rect.getY() + rect.getHeight() / 2);
+            bDef.position.set((rect.getX() + rect.getWidth() / 2) / MyGdxGame.PPM, (rect.getY() + rect.getHeight() / 2) / MyGdxGame.PPM);
 
             body = world.createBody(bDef);
 
-            shape.setAsBox(rect.getWidth() / 2, rect.getHeight() / 2);
+            shape.setAsBox(rect.getWidth() / 2 / MyGdxGame.PPM, rect.getHeight() / 2 / MyGdxGame.PPM);
             fDef.shape = shape;
 
             body.createFixture(fDef);
@@ -73,11 +79,11 @@ public class PlayScreen implements Screen {
         for (MapObject object : map.getLayers().get(3).getObjects().getByType(RectangleMapObject.class)) {
             Rectangle rect = ((RectangleMapObject) object).getRectangle();
             bDef.type = BodyDef.BodyType.StaticBody;
-            bDef.position.set(rect.getX() + rect.getWidth() / 2, rect.getY() + rect.getHeight() / 2);
+            bDef.position.set((rect.getX() + rect.getWidth() / 2) / MyGdxGame.PPM, (rect.getY() + rect.getHeight() / 2) / MyGdxGame.PPM);
 
             body = world.createBody(bDef);
 
-            shape.setAsBox(rect.getWidth() / 2, rect.getHeight() / 2);
+            shape.setAsBox(rect.getWidth() / 2 / MyGdxGame.PPM, rect.getHeight() / 2 / MyGdxGame.PPM);
             fDef.shape = shape;
 
             body.createFixture(fDef);
@@ -87,11 +93,11 @@ public class PlayScreen implements Screen {
         for (MapObject object : map.getLayers().get(5).getObjects().getByType(RectangleMapObject.class)) {
             Rectangle rect = ((RectangleMapObject) object).getRectangle();
             bDef.type = BodyDef.BodyType.StaticBody;
-            bDef.position.set(rect.getX() + rect.getWidth() / 2, rect.getY() + rect.getHeight() / 2);
+            bDef.position.set((rect.getX() + rect.getWidth() / 2) / MyGdxGame.PPM, (rect.getY() + rect.getHeight() / 2) / MyGdxGame.PPM);
 
             body = world.createBody(bDef);
 
-            shape.setAsBox(rect.getWidth() / 2, rect.getHeight() / 2);
+            shape.setAsBox(rect.getWidth() / 2 / MyGdxGame.PPM, rect.getHeight() / 2 / MyGdxGame.PPM);
             fDef.shape = shape;
 
             body.createFixture(fDef);
@@ -101,11 +107,11 @@ public class PlayScreen implements Screen {
         for (MapObject object : map.getLayers().get(4).getObjects().getByType(RectangleMapObject.class)) {
             Rectangle rect = ((RectangleMapObject) object).getRectangle();
             bDef.type = BodyDef.BodyType.StaticBody;
-            bDef.position.set(rect.getX() + rect.getWidth() / 2, rect.getY() + rect.getHeight() / 2);
+            bDef.position.set((rect.getX() + rect.getWidth() / 2) / MyGdxGame.PPM, (rect.getY() + rect.getHeight() / 2) / MyGdxGame.PPM);
 
             body = world.createBody(bDef);
 
-            shape.setAsBox(rect.getWidth() / 2, rect.getHeight() / 2);
+            shape.setAsBox(rect.getWidth() / 2 / MyGdxGame.PPM, rect.getHeight() / 2 / MyGdxGame.PPM);
             fDef.shape = shape;
 
             body.createFixture(fDef);
@@ -120,13 +126,22 @@ public class PlayScreen implements Screen {
     }
 
     private void handleInput(float delta) {
-        if (Gdx.input.isTouched())
-            gameCam.position.x += 100 * delta;
+        if (Gdx.input.isKeyJustPressed(Input.Keys.UP))
+            mario.body.applyLinearImpulse(new Vector2(0, 4f), mario.body.getWorldCenter(), true);
+        if (Gdx.input.isKeyJustPressed(Input.Keys.RIGHT) && mario.body.getLinearVelocity().x <= 2)
+            mario.body.applyLinearImpulse(new Vector2(0.1f, 0), mario.body.getWorldCenter(), true);
+        if (Gdx.input.isKeyJustPressed(Input.Keys.LEFT) && mario.body.getLinearVelocity().x <= -2)
+            mario.body.applyLinearImpulse(new Vector2(-0.1f, 0), mario.body.getWorldCenter(), true);
+
+
     }
 
     private void update(float delta) {
         handleInput(delta);
 
+        world.step(1 / 60f, 6, 2);
+
+        gameCam.position.x = mario.body.getPosition().x;
         gameCam.update();
         renderer.setView(gameCam);
     }
---
+++ core/src/ch/noseryoung/gameexampmle/Sprites/Mario.java
@@ -0,0 +1,36 @@
+package ch.noseryoung.gameexampmle.Sprites;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.World;
+
+import ch.noseryoung.gameexampmle.MyGdxGame;
+
+public class Mario extends Sprite {
+    public World world;
+    public Body body;
+
+    public Mario(World world) {
+        this.world = world;
+        defineMario();
+    }
+
+    public void defineMario() {
+        BodyDef bdef = new BodyDef();
+        bdef.position.set(32/ MyGdxGame.PPM, 32/MyGdxGame.PPM);
+        bdef.type = BodyDef.BodyType.DynamicBody;
+        body = world.createBody(bdef);
+
+        FixtureDef fdef = new FixtureDef();
+        CircleShape shape = new CircleShape();
+        shape.setRadius(5/MyGdxGame.PPM);
+
+        fdef.shape = shape;
+        body.createFixture(fdef);
+
+    }
+
+}
